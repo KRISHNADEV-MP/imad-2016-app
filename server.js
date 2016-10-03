2e9bd355cbbc520289777d6ca082bf3f44aac9a7
KRISHNADEV <mpkrishnadev@gmail.com>
2016-10-03 20:37:37 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.get('/', function (req, res) {
 });
 
 app.get('/article-one', function (req, res) {
-  res_send('Article one requested');
+  send('Article one requested');
 });
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
